31d9a0e9e60e77abdb82a0d327f33d77e7960e88
NewLuo <4970189+CuminLo@users.noreply.github.com>
2025-07-22 18:33:23 +0800

    add

--- brew/setup_macos.sh
+++ brew/setup_macos.sh
@@ -283,7 +283,7 @@ main() {
   local casks=(
     "google-chrome" "iterm2" "orbstack" "itsycal" "clipy" "rectangle"
     "visual-studio-code" "trae-cn" "tabby" "fork" "stats" "localsend"
-    "easydict" "pixpin" "wechat" "utools" "jordanbaird-ice"
+    "easydict" "pixpin" "wechat" "utools" "jordanbaird-ice" "skim"
   )
 
   # 3. 执行安装
