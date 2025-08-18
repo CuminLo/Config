5217d0683ca6cdfbc1959ad7006d19ec00feb3c3
NewLuo <4970189+CuminLo@users.noreply.github.com>
2025-08-18 09:30:48 +0800

    update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -8,21 +8,6 @@ alias la='eza -a --icons'
 alias ll='eza -al --icons --group-directories-first'
 alias code='trae'
 
-# 定义 update 函数
-update() {
-    local original_dir=$(pwd)
-    brew update
-    brew upgrade
-    if command_exists tldr; then
-        tldr --update
-        tldr --clean-cache
-    fi
-    if [ -d "$HOME/Library/Rime" ]; then
-        cd "$HOME/Library/Rime" && git pull -r
-    fi
-    cd "$original_dir"
-}
-
 rime_update() {
     # --- 更新 Rime 設定 (如果目錄存在) ---
     local rime_dir="$HOME/Library/Rime"
@@ -38,6 +23,21 @@ rime_update() {
     else
         echo "--- 未找到 Rime 設定目錄 ($rime_dir)，跳過更新。 ---"
     fi
+}
+
+tldr_update() {
+    tldr --clean-cache
+    tldr --update
+}
+
+# 定义 update 函数
+update() {
+    local original_dir=$(pwd)
+    brew update && brew outdate
+    brew upgrade
+    tldr_update
+    rime_update
+    cd "$original_dir"
 }
 
 #goenv_update() {
